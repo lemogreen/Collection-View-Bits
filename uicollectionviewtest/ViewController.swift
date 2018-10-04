35919c0d428a8584320467514f0c09d6fa2048dc
Lemo_Green <lemogreen@gmail.com>
2018-10-04 09:39:08 +0700

    Initial Commit

---
+++ uicollectionviewtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uicollectionviewtest
+//
+//  Created by Антон Коваленко on 04/10/2018.
+//  Copyright © 2018 Антон Коваленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
